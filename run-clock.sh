b738280c1d8b9c956a19eb383e63c686e7e4c8b1
prehensile <me@prehensile.co.uk>
2017-04-25 21:46:10 +0100

    added pwd setting to run-clock script

--- run-clock.sh
+++ run-clock.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-sudo ./src/clock.py
+# get the absolute path to this script, cribbed from:
+# http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $SCRIPTPATH/src
+./clock.py
+
+
